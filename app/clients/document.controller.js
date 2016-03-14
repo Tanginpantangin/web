74e92560804dce95f209e2d31aae21ce5ae68d9a
Vinh Luu <vinhlt3@gmail.com>
2016-03-13 23:10:37 -0400

    Implement document page

--- app/clients/document.controller.js
+++ app/clients/document.controller.js
@@ -3,6 +3,40 @@
 
 angular.module('app')
     .controller('DocumentController', [function() {
-        var self = this;
-        self.message = 'Documents page';
+        var ctrl = this;
+        ctrl.documents = [{
+            nameKey: 'Inalang_Aymoney_Name',
+            descriptionKey: 'Inalang_Aymoney_Description',
+            image: 'clients/assets/documents/DictionaryCabatonAymonierAmandine.png',
+            file: 'clients/assets/documents/DictionaryCabatonAymonierAmandine.pdf',
+            fileName: 'DictionaryCabatonAymonierAmandine.pdf'
+        },
+        {
+            nameKey: 'Inalang_Mousay_Name',
+            descriptionKey: 'Inalang_Mousay_Description',
+            image: 'clients/assets/documents/TuDienMoussay.png',
+            file: 'clients/assets/documents/TuDienMoussay.pdf',
+            fileName: 'TuDienMoussay.pdf'
+        },
+        {
+            nameKey: 'Xakawi_Cam_Name',
+            descriptionKey: 'Xakawi_Cam_Description',
+            image: 'clients/assets/documents/Xakawi_Thun_kra_dal_2016.png',
+            file: 'clients/assets/documents/Xakawi_Thun_kra_dal_2016.pdf',
+            fileName: 'Xakawi_Thun_kra_dal_2016.pdf'
+        },
+        {
+            nameKey: 'Kadha_adaoh_Cam_Name',
+            descriptionKey: 'Kadha_adaoh_Cam_Description',
+            image: 'clients/assets/documents/kadha_adaoh_cam.png',
+            file: 'clients/assets/documents/kadha_adaoh_cam.pdf',
+            fileName: 'kadha_adaoh_cam.pdf'
+        },
+        {
+            nameKey: 'Ebook_bac_Akhar_Cam_Name',
+            descriptionKey: 'Ebook_bac_Akhar_Cam_Description',
+            image: 'clients/assets/documents/AkharThrah7Harei.png',
+            file: 'clients/assets/documents/AkharThrah7Harei.pdf',
+            fileName: 'AkharThrah7Harei.pdf'
+        }];
     }]);
