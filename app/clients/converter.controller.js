880daafe3a731c8d9b7f14ecc81bfc7d90e4683f
Hoangtri <kauthara2000@gmail.com>
2016-03-18 21:07:01 +0700

    xalih hoang tri

--- app/clients/converter.controller.js
+++ app/clients/converter.controller.js
@@ -5,4 +5,10 @@ angular.module('app')
     .controller('ConverterController', [function() {
         var self = this;
         self.message = 'Convert page';
+        self.akhars = [
+            { id: 1, name: 'Akhar BBC' },
+            { id: 2, name: 'Akhar Thrah' },
+            { id: 3, name: 'Rumi BBC' }
+          ];
+          self.selectedAkhar = { id: 1, name: 'Akhar BBC' };
     }]);
