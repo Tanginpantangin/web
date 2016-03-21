b64355b0100f4e621b22bb0e66750cba7ee03a15
Hoangtri <kauthara2000@gmail.com>
2016-03-22 01:47:27 +0700

    over view - commit by hoang tri dong

--- app/clients/converter.controller.js
+++ app/clients/converter.controller.js
@@ -2,6 +2,29 @@
 'use strict';
 
 angular.module('app')
+      .directive('autoheight', function() {
+          return function(scope, element, attrs) {
+            document.getElementById(element[0].id).addEventListener('keyup',function() {
+                this.style.overflow = 'hidden';
+
+                if(this.scrollHeight > 154)
+                {
+                  this.style.height =  this.scrollHeight + 'px';
+                };
+                if(this.scrollHeight > 600)
+                {
+                    this.style.overflow='auto';
+                      this.style.height = "600px";
+                }
+                if(this.scrollHeight<154)
+                {
+                        this.style.overflow = 'hidden';
+                    this.style.height = "154px";
+                }
+            }, false);
+
+          };
+      })
     .controller('ConverterController', [function() {
         var self = this;
         self.message = 'Convert page';
@@ -11,5 +34,5 @@ angular.module('app')
             { id: 3, name: 'Rumi BBC' }
           ];
           self.selectedAkhar = { id: 1, name: 'Akhar BBC' };
-          
+
     }]);
