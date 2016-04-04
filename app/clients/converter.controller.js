2185a9076b030180264f3578f8c4d30f58978482
Hoangtri <kauthara2000@gmail.com>
2016-04-04 20:41:22 +0700

    complete converter -by hoang tri

--- app/clients/converter.controller.js
+++ app/clients/converter.controller.js
@@ -6,7 +6,6 @@ angular.module('app')
           return function(scope, element, attrs) {
             document.getElementById(element[0].id).addEventListener('keyup',function() {
                 this.style.overflow = 'hidden';
-
                 if(this.scrollHeight > 154)
                 {
                   this.style.height =  this.scrollHeight + 'px';
@@ -39,99 +38,65 @@ angular.module('app')
             { id: 8, name: 'Latin tuei Inrasara'},
             { id: 9, name: 'Latin Kawom Tuek Tuah'}
           ];
-          self.destinationList = [
-              { id: 1, name: 'AT Font Yapata'},
-              { id: 2, name: 'AT Font Gilai Praong'},
-              { id: 3, name: 'AT Font Cam EFEO'},
-              { id: 4, name: 'AT Kawom Tuek Tuah'},
-              { id: 5, name: 'AT Unicode Cam kar'},
-              { id: 6, name: 'TA Unicode Cam VN'},
-              { id: 7, name: 'Latin tuei EFEO'},
-              { id: 8, name: 'Latin tuei Inrasara'},
-              { id: 9, name: 'Latin Kawom Tuek Tuah'}
-            ];
-
+          self.destinationList =  angular.copy(self.sourceList);
+          //This is Source
           self.indexSource = 0;// as IsDefause
-          self.setIndexSource = function(index) {
-              self.indexSource = index;
+          self.setIndexSource = function(index) {// event click button
+                  self.indexSource = index;
                   if(self.indexSource==self.indexDestination){
-                      if(self.indexSource==0)
-                      {
-                        self.indexDestination= self.indexDestination+1;
-                      }
-                      else{
-                                if(self.indexSource==1)
-                                {
-                                    self.indexDestination= self.indexDestination-1;
-                                }
-                                else {
-                                    if(self.sourceList[self.indexSource].id==self.destinationList[2].id)
-                                    {
-                                          self.indexDestination= self.indexDestination-1;
-                                    }
-                                }
-                      }
+                      self.indexDestination = moveActive(self.indexSource,self.indexDestination);
                   }
           };
           self.selectItemSource = function(index)//select dropdownlist
           {
-
-              index = index+3; //fist position in list dropdownlist have position=3 in akhars ;
-              var temp = self.sourceList[index];
-              self.sourceList[index] = self.sourceList[2];
-              self.sourceList[2] = temp;
+              self.indexDestination=selectItemConvert(index,self.sourceList,self.destinationList,self.indexDestination);
               self.indexSource = 2;
-              if(self.sourceList[self.indexSource].id==self.destinationList[2].id)
-              {
-                    self.indexDestination= self.indexDestination-1;
-              }
           };
-
-
+          // this is Destination
             self.indexDestination = 1;// as IsDefause
-            self.setIndexDestination = function(index) {
+            self.setIndexDestination = function(index) {// event click button
                 self.indexDestination = index;
                 if(self.indexDestination==self.indexSource){
-                    if(self.indexDestination==0)
-                    {
-                      self.indexSource= self.indexSource+1;
-                    }
-                    else{
-                              if(self.indexDestination==1)
-                              {
-                                  self.indexSource= self.indexSource-1;
-                              }
-                              else {
-                                  if(self.destinationList[self.indexDestination].id==self.sourceList[2].id)
-                                  {
-                                        self.indexSource= self.indexSource-1;
-                                  }
-                              }
-                    }
+                    self.indexSource = moveActive(self.indexDestination,self.indexSource);
                 }
             };
             self.selectItemDestination = function(index)//select dropdownlist
             {
-                index = index+3; //fist position in list dropdownlist have position=3 in akhars ;
-                var temp = self.destinationList[index];
-                self.destinationList[index] = self.destinationList[2];
-                self.destinationList[2] = temp;
-                self.indexDestination = 2;
-                if(self.destinationList[self.indexDestination].id==self.sourceList[2].id)
-                {
-                      self.indexSource= self.indexSource-1;
-                }
+              self.indexSource=selectItemConvert(index,self.destinationList,self.sourceList,self.indexSource);
+              self.indexDestination = 2;
             };
             self.changeConvert = function(){
-
-
-
-             var tempList = self.destinationList;
-            self.destinationList=self.sourceList;
-            self.sourceList = tempList;
-            var temp = self.indexSource;
-           self.indexSource=self.indexDestination;
-           self.indexDestination = temp;
+              var tempList = self.destinationList;
+              self.destinationList = self.sourceList;
+              self.sourceList = tempList;
+              var temp = self.indexSource;
+              self.indexSource=self.indexDestination;
+              self.indexDestination = temp;
+            };
+            Array.prototype.swap = function(a, b){
+              var temp = this[a];
+              this[a] = this[b];
+              this[b] = temp;
+            };
+            var moveActive = function(index1, index2){
+              if(index1==0)
+              {
+                return index2+1;
+              }
+              if((index1==1)||(self.sourceList[2].id==self.destinationList[2].id))
+              {
+                return index2-1;
+              }
+              return index2;
+            };
+            var selectItemConvert = function(index,listMain,listCondition,indexCondition, isIndexSource){
+              index = index+3;//fist position in list dropdownlist have position=3
+              listMain.swap(index,2);
+              if(listMain[2].id==listCondition[2].id)
+              {
+                    indexCondition= indexCondition-1;
+              }
+              return indexCondition;
             };
 
     }]);
