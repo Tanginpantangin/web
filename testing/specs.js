79739c992515b19db3b701d21557a0f485cf0efa
Jayam Padra <hoangminhgiam88@gmail.com>
2016-04-18 11:23:50 +0700

    Add test case from transliteration to Cam

--- testing/specs.js
+++ testing/specs.js
@@ -28,6 +28,7 @@ var assert = function(source, expected) {
     expect(elm.getText()).toEqual(expected);
 };
 
+/*
 describe('Convert page', function() {
     beforeEach(function() {
         // Open convert page
@@ -69,7 +70,9 @@ describe('Convert page', function() {
             '-k^ kN c@K -\\k" =g* m{N kR d} t`N x=I, tO&K t=k =m k% ad] b&] =h. -OH -O<K O)K d} -j`@N p=d, x=I -s" ad] -Q\' k*w S@H =S.');
     });
 });
+*/
 
+/*
 describe('Convert from Cham to transliteration', function() {
     beforeEach(function() {
         // Open convert page
@@ -153,3 +156,129 @@ describe('Convert from transliteration to transliteration', function() {
         assert(source, expected);
     });
 });
+*/
+
+describe('Convert from transliteration to Akhar Thrah', function() {
+    beforeEach(function() {
+        // Open convert page
+        browser.get('http://localhost:8080/#/converter', 20000);
+    });
+
+    var select = function(soureType, destinationType) {
+
+        // Select EFEO transliteration
+        element(by.id('source-more')).click();
+        element(by.id('source-' + soureType)).click();
+
+        // Select Yapata font
+        element(by.id('destination-more')).click();
+        element(by.id('destination-' + destinationType)).click();
+    };
+
+    it('should convert from transliteration EFEO to FontYapata', function() {
+        select(convertType.TransCamEFEO, convertType.FontYapata);
+        var source = 'Tamâkai yamân, yamân mbluak xaradang piéh cang xa-ai.';
+        var expected = 'tm=k ymN, ymN O)K xrd/ -p`@H c/ x=I.';
+        assert(source, expected);
+    });
+
+  it('should convert from transliteration Inrasara to FontYapata', function() {
+        select(convertType.TransInrasara, convertType.FontYapata);
+        var source = 'Tamưkai yamưn, yamưn bblwak xaradang pieh cang xa-ai.';
+        var expected = 'tm=k ymN, ymN O)K xrd/ -p`@H c/ x=I.';
+        assert(source, expected);
+    });
+
+    it('should convert from transliteration KTT to FontYapata', function() {
+        select(convertType.TransKawomTT, convertType.FontYapata);
+        var source = 'Tamưkai yamưưn, yamưưn bblwak xaradang piêh cang xa-ai.';
+        var expected = 'tm=k ymN, ymN O)K xrd/ -p`@H c/ x=I.';
+        assert(source, expected);
+    });
+});
+
+
+/* Waiting!!!!!!!!!!!!!!! */
+/*
+describe('Convert from Akhar Thrah to Akhar Thrah', function() {
+    beforeEach(function() {
+        // Open convert page
+        browser.get('http://localhost:8080/#/converter', 20000);
+    });
+
+    var select = function(soureType, destinationType) {
+
+        // Select EFEO transliteration
+        element(by.id('source-' + soureType)).click();
+
+        // Select Yapata font
+        element(by.id('destination-more')).click();
+        element(by.id('destination-' + destinationType)).click();
+    };
+
+    it('should convert from font Yapata to EFEO transliteration', function() {
+        select(convertType.FontYapata, convertType.TransCamEFEO);
+        var source = 'hj%N bL l{a&%, r~%P \\t# a`% am% l{aN y~H y~H. pQ`K b{l%N y&K a=m%K -n< -m<T -a< AN oH z~Y E{R. O$ -k`# O$ a;%N a=m%K am% hl%R t`N. mr%T s% jl%N -r"< an%K -n< bC. l{-gY ZK apH l{gH ZK cd&%% oH md% O&H kR. \\bH p=d m=\\EY -j`@N -QC v[ a=m%K am% an{T p-y@R. xn/ atH c`{P rOH J%K dh(w s`\' hd]. eL r~%P \\d] hl] b~w tL. a^N =z \\p" -g*" an%K l{k~w Q&% -n", E~N aqK b{lN hd], a`% hr] hdH h=d, an%K w@K g{lC =m -F%K l{E] -F%K a`%, s% r{t~H E~N th% a=m%K am% b~Y t`N. s/ F-Q^ pg% jl@H o% m" \\d@H y~w ur/. m{N a=m%K am% t`N h% -hY \\p" k% an%K r-l%. xn/ atH c`{P rOH -p`@H k% jl%N hd]. wR r~%P \\d] k% an%K dH -O<K.';
+        var expected = 'hajan bal liaua, rup trem aia amâ lian yuh yuh. pandiak bilan yuak amaik nao maot ao khan oh nguy thir. mbeng kiém mbeng ân amaik amâ halar tian. mârat sa jalan raong anâk nao bac. ligoy ngak apah ligah ngak cadua oh mâda mbuah kar. brah padai mâthraiy jién ndoc nyim amaik amâ anit payér. xanâng atah ciip rambah jhak dahluw siam hadei. él rup drei halei buw tal. aen ngai praong glaong anâk likuw ndua naong, thun anak bilan hadei, aia harei hadah hadai, anâk wek gilac mai ppok lithei ppok aia, sa rituh thun taha amaik amâ buy tian. sang ppandé paga jaleh o maong dreh yuw urang. min amaik amâ tian ha hoy praong ka anâk ralo. xanâng atah ciip rambah piéh ka jalan hadei. war rup drei ka anâk dah mbaok.';
+        assert(source, expected);
+    });
+
+    var select = function(soureType, destinationType) {
+
+        // Select Akhar Thrah font source
+        //element(by.id('source-more')).click();
+        //element(by.id('source-' + soureType)).click();
+        element(by.id('source-0')).click();
+
+        // Select Akhar Thrah font destination
+        //element(by.id('destination-more')).click();
+        //element(by.id('destination-' + destinationType)).click();
+        element(by.id('destination-7')).click();
+    };
+
+    it('should convert from font Yapata to EFEO transliteration', function() {
+        select(convertType.FontYapata, convertType.TransCamEFEO);
+        var source = 'hj%N bL l{a&%, r~%P \\t# a`% am% l{aN y~H y~H. pQ`K b{l%N y&K a=m%K -n< -m<T -a< AN oH z~Y E{R. O$ -k`# O$ a;%N a=m%K am% hl%R t`N. mr%T s% jl%N -r"< an%K -n< bC. l{-gY ZK apH l{gH ZK cd&%% oH md% O&H kR. \\bH p=d m=\\EY -j`@N -QC v[ a=m%K am% an{T p-y@R. xn/ atH c`{P rOH J%K dh(w s`\' hd]. eL r~%P \\d] hl] b~w tL. a^N =z \\p" -g*" an%K l{k~w Q&% -n", E~N aqK b{lN hd], a`% hr] hdH h=d, an%K w@K g{lC =m -F%K l{E] -F%K a`%, s% r{t~H E~N th% a=m%K am% b~Y t`N. s/ F-Q^ pg% jl@H o% m" \\d@H y~w ur/. m{N a=m%K am% t`N h% -hY \\p" k% an%K r-l%. xn/ atH c`{P rOH -p`@H k% jl%N hd]. wR r~%P \\d] k% an%K dH -O<K.';
+        var expected = 'hajan bal liaua, rup trem aia amâ lian yuh yuh. pandiak bilan yuak amaik nao maot ao khan oh nguy thir. mbeng kiém mbeng ân amaik amâ halar tian. mârat sa jalan raong anâk nao bac. ligoy ngak apah ligah ngak cadua oh mâda mbuah kar. brah padai mâthraiy jién ndoc nyim amaik amâ anit payér. xanâng atah ciip rambah jhak dahluw siam hadei. él rup drei halei buw tal. aen ngai praong glaong anâk likuw ndua naong, thun anak bilan hadei, aia harei hadah hadai, anâk wek gilac mai ppok lithei ppok aia, sa rituh thun taha amaik amâ buy tian. sang ppandé paga jaleh o maong dreh yuw urang. min amaik amâ tian ha hoy praong ka anâk ralo. xanâng atah ciip rambah piéh ka jalan hadei. war rup drei ka anâk dah mbaok.';
+        assert(source, expected);
+    });
+
+    it('should convert from FontYapata to FontGilaiPraong', function() {
+        select(convertType.FontYapata, convertType.TransInrasara);
+        var source = '=m rw$ pl] ad] =h x=I -n< s% jl%N O$ ikN s% d-n<.';
+        var expected = '=m rw$ pl] ad] =h x=i _n< s% jl%N O$ IkN s% d_n<.';
+        assert(source, expected);
+    });
+
+
+    it('should convert from FontYapata to FontCamEFEO', function() {
+        select(convertType.FontYapata, convertType.FontCamEFEO);
+        var source = '=m rw$ pl] ad] =h x=I -n< s% jl%N O$ ikN s% d-n<.';
+        var expected = '`m rv+! plE adE `h s`A On@ S_ jl_. Ó+! ik. S_ dOn@.';
+        assert(source, expected);
+    });
+
+    it('should convert from FontYapata to FontKTT', function() {
+        select(convertType.FontYapata, convertType.FontKTT);
+        var source = '=m rw$ pl] ad] =h x=I -n< s% jl%N O$ ikN s% d-n<.';
+        var expected = '=y ;,I u'S :pS =/ .=? -r$ {^ i'^R [I "aR {^ p-r$.';
+        assert(source, expected);
+    });
+
+
+    it('should convert from FontYapata to FontUniCamKur', function() {
+        select(convertType.FontYapata, convertType.FontUniCamKur);
+        var source = '=m rw$ pl] ad] =h x=I -n< s% jl%N O$ ikN s% d-n<.';
+        var expected = 'ꨰꨟ ꨣꨥꨮꩃ ꨚꨤꨬ ꨀꨕꨬ ꨰꨨ ꨦꨰꨄ ꨯꨗꨱ ꨧꨩ ꨎꨤꨩꩆ ꨡꨮꩃ ꨁꨆꩆ ꨧꨩ ꨕꨯꨗꨱ.';
+        assert(source, expected);
+    });
+
+    it('should convert from FontYapata to FontUniCamVN', function() {
+        select(convertType.FontYapata, convertType.FontUniCamVN);
+        var source = '=m rw$ pl] ad] =h x=I -n< s% jl%N O$ ikN s% d-n<.';
+        var expected = 'ꨰꨟ ꨣꨥꨮꩃ ꨚꨤꨬ ꨀꨕꨬ ꨰꨨ ꨦꨰꨄ ꨯꨗꨱ ꨧꨩ ꨎꨤꨩꩆ ꨡꨮꩃ ꨁꨆꩆ ꨧꨩ ꨕꨯꨗꨱ.';
+        assert(source, expected);
+    });
+
+});
+*/
